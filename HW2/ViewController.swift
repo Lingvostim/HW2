58691799242ecee706de5ade7c99c4bdb0b89b98
kirill <golub.kirill12@gmail.com>
2023-09-25 15:55:59 +0300

    светофор

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+
+    @IBOutlet weak var orangeSignalView: UIView!
+    @IBOutlet weak var greenSignalView: UIView!
+
+    @IBOutlet weak var redSignalView: UIView!
+    var signalViews: [UIView] = []
+    var currentSignalIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signalViews = [redSignalView, orangeSignalView, greenSignalView]
+
+                // Выключаем все сигналы при запуске
+                for signalView in signalViews {
+                    signalView.alpha = 0.3
+                    signalView.layer.cornerRadius = signalView.frame.size.width / 2
+                }
+
+                // Настраиваем кнопку
+                startButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func startButonTapped() {
+        signalViews[currentSignalIndex].alpha = 0.3
+                currentSignalIndex = (currentSignalIndex + 1) % signalViews.count
+                signalViews[currentSignalIndex].alpha = 1.0
+
+                // Меняем заголовок кнопки
+                if currentSignalIndex == 0 {
+                    startButton.setTitle("START", for: .normal)
+                } else {
+                    startButton.setTitle("NEXT", for: .normal)
+                }
+    }
 }
 
